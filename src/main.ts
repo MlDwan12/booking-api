3a38a5b08dd05b71947f133a5fc2bce32a75ad73
MlDwan12 <ggdd121212@bk.ru>
2024-10-24 17:03:25 +0300

    добавлена первоначальная настройка документирования

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const port = process.env.PORT;
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('Booking')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document); // Путь для доступа к Swagger UI
 
   await app.listen(port ?? 3000, () =>
     console.log(`The server is running on ${port} port`),
